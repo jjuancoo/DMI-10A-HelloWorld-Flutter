c94e0397e459fa3b5bdafcfc175024cc9bd0b7de
Juanco <210373@utxicotepec.edu.mx>
2024-10-24 11:56:14 -0600

    Practica 21
    Crea la app del contador y primeros pasos con los estilos

---
+++ helloworld_app_210373/lib/presentation/screens/counter_screen.dart
@@ -0,0 +1,56 @@
+import 'package:flutter/material.dart';
+
+class CounterScreen extends StatefulWidget {
+  const CounterScreen({super.key});
+
+  @override
+  State<CounterScreen> createState() => _CounterScreenState();
+}
+
+int clickCounter = 0;
+
+//TODO: crear una variable para el contador y un botón para reiniciar el contador
+//TODO: Tarea quitar la s cuando un clik y cuando sea más
+
+class _CounterScreenState extends State<CounterScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Contador 210373'),
+        actions: [
+          IconButton(
+            onPressed: () {
+              // Handle reset button press
+            },
+            icon: const Icon(Icons.arrow_back_ios),
+          ),
+        ],
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              '$clickCounter',
+              style:
+                  const TextStyle(fontSize: 150, fontWeight: FontWeight.w400),
+            ),
+            const Text(
+              'Clicks',
+              style: TextStyle(fontSize: 25, fontWeight: FontWeight.w400),
+            )
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          setState(() {
+            clickCounter++;
+          });
+        },
+        child: const Icon(Icons.add),
+      ),
+    );
+  }
+}
